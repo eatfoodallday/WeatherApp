6cec01afc26ed8484b2246bf431b58b37ba28a2e
Jordan Yen <Jordan Yen>
2016-08-29 00:44:57 -0700

    fourth commit

---
+++ app/helpers/api.js
@@ -0,0 +1,48 @@
+var axios = require('axios');
+
+var _baseURL = 'http://api.openweathermap.org/data/2.5/';
+var _APIKEY = '491530b9d24e5f98e43cf1e5ea89d546';
+
+function prepRouteParams (queryStringData) {
+  return Object.keys(queryStringData)
+    .map(function (key) {
+      return key + '=' + encodeURIComponent(queryStringData[key]);
+    }).join('&')
+}
+
+function prepUrl (type, queryStringData) {
+  return _baseURL + type + '?' + prepRouteParams(queryStringData);
+}
+
+function getQueryStringData (city) {
+  return {
+    q: city,
+    type: 'accurate',
+    APPID: _APIKEY,
+    cnt: 5
+  }
+}
+
+function getCurrentWeather (city) {
+  var queryStringData = getQueryStringData(city);
+  var url = prepUrl('weather', queryStringData)
+
+  return axios.get(url)
+    .then(function (currentWeatherData) {
+      console.log(currentWeatherData.data)
+    })
+}
+
+function getForecast (city) {
+  var queryStringData = getQueryStringData(city);
+  var url = prepUrl('forecast/daily', queryStringData)
+
+  return axios.get(url)
+    .then(function (forecastData) {
+      console.log(forecastData.data)
+    })
+}
+
+module.exports = {
+  getCurrentWeather: getCurrentWeather,
+  getForcast: getForecast
